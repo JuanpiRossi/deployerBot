f45a4498eac48cef55980416fcb43300e9c82914
JuanpiRossi <rossi_juanpablo@hotmail.com>
2018-12-20 13:46:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ TOKEN = tokenConfig.BOT_TOKEN
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
-def echo(bot,update):
-    if str(update.message.from_user.id) == "528527409":
-        os.system("sh /var/sources/deployer/deployMetegolBot")
-        bot.send_message(chat_id=update.message.chat_id,text="deployeado!")
+allowed_users = ["528527409"]
+
+def echo(bot,update,args):
+    if str(update.message.from_user.id) in allowed_users:
+        if args[0] == "metegol":
+            os.system("sh /var/sources/deployer/deployMetegolBot")
+            bot.send_message(chat_id=update.message.chat_id,text="deployeado!")
+        else:
+            bot.send_message(chat_id=update.message.chat_id,text="No se encontro la tarea de deploy: " + str(args[0]))
     else:
         bot.send_message(chat_id=update.message.chat_id,text="Falta autenticacion")
 
-test_handler = CommandHandler('deploy',echo)
+test_handler = CommandHandler('deploy',echo, pass_args=True)
 
 dispatcher.add_handler(test_handler)
 
